09b304a1c42ed347dd3c684c86c56cdc473e7e08
vitor sampaio <vitorsampaio.tds2@gmail.com>
2025-03-18 09:31:30 -0300

    feat: create footer component

--- src/app/titulos/page.jsx
+++ src/app/titulos/page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./titulos.module.css";
 import Header from "../components/header";
+import Footer from "../components/footer/footer"
 
 const CorinthiansTrophiesPage = () => {
   return (
@@ -216,37 +217,10 @@ const CorinthiansTrophiesPage = () => {
         </section>
       </main>
 
-      <footer className={styles.footer}>
-        <div className={styles.footerContent}>
-          <div className={styles.footerSection}>
-            <h3>Sobre o Sport Club Corinthians Paulista</h3>
-            <p>
-              Fundado em 1910, o Corinthians é um dos clubes mais populares e
-              vitoriosos do futebol brasileiro, com milhões de torcedores ao
-              redor do mundo.
-            </p>
-          </div>
-          <div className={styles.footerSection}>
-            <h3>Contato</h3>
-            <p>Email: contato@corinthians.com.br</p>
-            <p>Telefone: (11) 2095-3000</p>
-          </div>
-          <div className={styles.footerSection}>
-            <h3>Siga-nos</h3>
-            <div className={styles.socialLinks}>
-              <a href="#">Facebook</a>
-              <a href="#">Instagram</a>
-              <a href="#">Twitter</a>
-            </div>
-          </div>
-        </div>
-        <div className={styles.copyright}>
-          <p>
-            &copy; 2025 Sport Club Corinthians Paulista. Todos os direitos
-            reservados.
-          </p>
-        </div>
-      </footer>
+
+<Footer />
+
+
     </div>
   );
 };
